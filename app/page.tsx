880c48a064c6087957af194df402010ae0788dea
Abhishek Sharma <2k22.cscys.2211373@gmail.com>
2025-04-14 16:31:39 +0530

    Added the icon for profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Navbar from "@/components/Navbar";
 import MainSection from "@/components/MainSection";
-import Footer from "@/components/Footer";
 import { SkillBadge } from "@/components/SkillBadges";
 
 export default function Home() {
